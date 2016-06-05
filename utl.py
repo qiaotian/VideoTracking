e2e62da2a3431e6e4586a9998a4e9034d209eee0
qiaotian <qiaotian@outlook.com>
2016-06-05 12:40:09 +0800

    add a tool to this resp

--- utl.py
+++ utl.py
@@ -2,7 +2,7 @@
 # @Date:   2016-06-05T10:21:02+08:00
 # @Email:  qiaotian@me.com
 # @Last modified by:   root
-# @Last modified time: 2016-06-05T12:35:36+08:00
+# @Last modified time: 2016-06-05T12:40:06+08:00
 # @License: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 
 
@@ -12,7 +12,7 @@ import numpy as np
 import argparse
 import time
 
-def crop(image, x, y, w, h):
+def roi(image, x, y, w, h):
     if image.ndim != 3:
         print('image dim invalid, crop failed')
         return
@@ -33,7 +33,7 @@ def main(arv=None):
     args = parser.parse_args()
 
     image = cv2.imread(args.image)
-    croped = crop(image, args.x, args.y, args.w, args.h)
+    croped = roi(image, args.x, args.y, args.w, args.h)
     if croped.shape[0]!=0:
         cv2.imwrite('./out/%s.jpg' % time.time(), croped)
     else:
