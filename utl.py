a04d89285e66fb1ed8a1d15a8276366927b186c0
qiaotian <qiaotian@outlook.com>
2016-06-13 00:18:55 +0800

    updates

--- utl.py
+++ utl.py
@@ -2,7 +2,7 @@
 # @Date:   2016-06-05T10:21:02+08:00
 # @Email:  qiaotian@me.com
 # @Last modified by:   root
-# @Last modified time: 2016-06-12T23:43:18+08:00
+# @Last modified time: 2016-06-13T00:15:49+08:00
 # @License: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 
 
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 import argparse
 import time
+import os
 
 def roi(image, x, y, w, h):
     if image.ndim != 3:
@@ -20,12 +21,15 @@ def roi(image, x, y, w, h):
         print('index beyond the boundry')
     return image[x:x+w-1, y:y+h-1, :]
 
-def crop_image(input_dir, output_dir, x, y, w, h):
+def crop_image(input_filename, output_filename, x, y, w, h):
     # TODO:
     return
 
-def crop_video(input_dir, output_dir, x, y, w, h):
-    videoCapture = cv2.VideoCapture(input_dir)
+def crop_image_batch(input_dir, output_dir, x, y, w, h):
+    return
+
+def crop_video(input_filename, output_filename, x, y, w, h):
+    videoCapture = cv2.VideoCapture(input_filename)
     fps = videoCapture.get(cv2.cv.CV_CAP_PROP_FPS)
     origin_size = (int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
                    int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
@@ -36,12 +40,11 @@ def crop_video(input_dir, output_dir, x, y, w, h):
     #fourcc = cv2.cv.CV_FOURCC(*'XVID')
     #fourcc = cv2.cv.FOURCC('M', 'P', '4', 'V')
     fourcc = cv2.cv.FOURCC('8', 'B', 'P', 'S')
-    videoWriter  = cv2.VideoWriter(output_dir, fourcc, fps, size, True)
+    videoWriter  = cv2.VideoWriter(output_filename, fourcc, fps, size, True)
 
     while(videoCapture.isOpened()):
         ret, frame = videoCapture.read()
         if ret==True:
-            cv2.imshow('frame',frame)
             videoWriter.write(frame[y:y+h, x:x+w])
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -49,6 +52,18 @@ def crop_video(input_dir, output_dir, x, y, w, h):
             break
     videoCapture.release()
     videoWriter.release()
+
+def crop_video_batch(input_dir, output_dir, x, y, w, h):
+    list_dirs = os.walk(input_dir)
+    for root, dirs, files in list_dirs:
+        for f in files:
+            print f
+            if f.endswith('mp4'):
+                if input_dir.endswith('/'):
+                    crop_video(input_dir+f, output_dir+'croped_'+f, x, y, w, h)
+                else:
+                    crop_video(input_dir+'/'+f, output_dir+'/croped_'+f, x, y, w, h)
+    return
 
 def main(arv=None):
     parser = argparse.ArgumentParser(
@@ -70,15 +85,26 @@ def main(arv=None):
     else:
         print('croped image is None')
     """
-    parser.add_argument('--input_dir', default='../../Desktop/MOVIE-0001.mp4')
-    parser.add_argument('--output_dir', default='../../Desktop/croped_MOVIE-0001.avi')
-    parser.add_argument('--x', type = int, default=300)
-    parser.add_argument('--y', type = int, default=300)
-    parser.add_argument('--w', type = int, default=50)
-    parser.add_argument('--h', type = int, default=50)
+    """
+    parser.add_argument('-i', '--input_filename', default='../ExperimentData/usdata/MOVIE-0001.mp4')
+    parser.add_argument('-o', '--output_filename', default='../ExperimentData/usdata/croped_MOVIE-0001.avi')
+    parser.add_argument('-x', '--origin_x', type = int, default=300)
+    parser.add_argument('-y', '--origin_y', type = int, default=300)
+    parser.add_argument('-w', '--width', type = int, default=50)
+    parser.add_argument('-h', '--height', type = int, default=50)
 
     args = parser.parse_args()
     crop_video(args.input_dir, args.output_dir, args.x, args.y, args.w, args.h)
+    """
+    parser.add_argument('--input_dir', default='../ExperimentData/usdata/', help='')
+    parser.add_argument('--output_dir', default='../ExperimentData/usdata/', help='')
+    parser.add_argument('--origin_x', type = int, default=300, help='')
+    parser.add_argument('--origin_y', type = int, default=300, help='')
+    parser.add_argument('--width', type = int, default=50, help='')
+    parser.add_argument('--height', type = int, default=50, help='')
+
+    args = parser.parse_args()
+    crop_video_batch(args.input_dir, args.output_dir, args.origin_x, args.origin_y, args.width, args.height)
 
 if __name__ == '__main__':
     main()
