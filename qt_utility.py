f82b4e74ef8e7a97156dd2b8915efb95ebdecaa7
qiaotian <qiaotian@outlook.com>
2016-06-13 10:11:56 +0800

    updates

--- qt_utility.py
+++ qt_utility.py
@@ -2,7 +2,7 @@
 # @Date:   2016-06-05T10:21:02+08:00
 # @Email:  qiaotian@me.com
 # @Last modified by:   root
-# @Last modified time: 2016-06-13T00:22:48+08:00
+# @Last modified time: 2016-06-13T10:11:46+08:00
 # @License: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 
 
