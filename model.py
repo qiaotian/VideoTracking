e1df2d4e1780697e3bd2d943fc1d2c0eb2fcede5
qiaotian <qiaotian@outlook.com>
2016-10-06 22:45:57 +0800

    update video tracking

---
+++ model.py
@@ -0,0 +1,22 @@
+""" Data Preprocess
+Author : QiaoTian
+Date   : 6th Oct 2016
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.ensemble import GradientBoostingClassifier as gbdt
+
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
